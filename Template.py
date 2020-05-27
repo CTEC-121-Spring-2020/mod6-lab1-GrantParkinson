32c2b7d59895b99f41941cdf8e085c47c3e40119
Grant Parkinson <grant.c.parkinson@gmail.com>
2020-05-27 12:19:14 -0700

    finished class

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
 <your name>
-<assignment/lab name>
+<Grant Parkinson>
 <assignment/lab description
 """
 
@@ -11,7 +11,26 @@ Process: description of what function does
 Output: return value and description
 """
 
-def main():
-    # code here
 
-main()    
+def main():
+    '''
+    # definite loop example
+    for i in range(0, 11, 2):
+        print(i)
+    print()
+
+    # same behavior but codes as indefinite or while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
+    print()
+    '''
+    print(True and False)
+    print(True and True)
+    print(True and False or False)
+    print(False or False or True)
+
+
+main()
